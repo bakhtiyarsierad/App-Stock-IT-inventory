d73d57b6404a3272ae476371bd5837eec9cd7935
Bakhtiyar <bakhtiyarsierad@gmail.com>
2020-09-21 21:10:21 +0700

    File update ada db

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $server   ="localhost" ;
 $username ="admin_stockit";
-$password ="Bestari008"; // Apabila tidak ada password ganti "1234" menjadi ""
+$password ="pass"; // Apabila tidak ada password ganti "1234" menjadi ""
 $database ="admin_stockit";
 
 $con = @mysqli_connect("$server", "$username", "$password", "$database");
